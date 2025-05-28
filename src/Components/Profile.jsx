073a103582f50e2945d27162b62346a1fef1eb60
michael <michaelmuturi15@gmail.com>
2025-05-28 12:57:21 +0300

    Profile.jsx edited

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  return <h1>Profile Page</h1>;
-};
+  const {currentUser, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/login");
+    } catch (error) {
+      console.error("Failed to log out:", error);
+    }
+  }
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      {currentUser ? (
+        <div>
+          <p>Email: {currentUser.email}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <p>You are not logged in.</p>
+      )}
+    </div>
+  );};
 
 export default Profile;
