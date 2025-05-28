e81b0a2306de99a2d4c7c7d2d997a58d350308a2
michael <michaelmuturi15@gmail.com>
2025-05-28 12:10:52 +0300

    firebase updated

--- src/Components/firebase.jsx
+++ src/Components/firebase.jsx
@@ -5,6 +5,9 @@ const firebaseConfig = {
     apiKey: "Group_9",
     authDomain: "group-9.firebaseapp.com",
     projectId: "group-9",
+    storageBucket: "group-9.appspot.com",
+    messagingSenderId: "1234567890",
+    appId: "group-9-app-id",
 };
 
 const app = initializeApp(firebaseConfig);
