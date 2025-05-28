91c915e95d85cc1c03761c7d259871bf9758f859
michael <michaelmuturi15@gmail.com>
2025-05-28 13:06:40 +0300

    changes to Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const Login = () => {
-  return <h1>Login Page</h1>;
-};
+  const {Login} = useAuth();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await Login(email, password);
+      navigate("/profile");
+    } catch (error) {
+      setError("Failed to log in. Please check your credentials.");
+    }
+  };  
+ 
+return(
+  <div>
+    <h1>Login Page</h1>
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </form>
+  </div>
+  );
+}
 
 export default Login;
