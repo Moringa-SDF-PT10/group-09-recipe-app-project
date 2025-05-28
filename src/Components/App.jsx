466f7d880b5d2ded8cd0afcd187079532f5516e4
mathiasmunene <mathiasmunene@gmail.com>
2025-05-28 18:06:14 +0300

    Changes in App.jsx and footer added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,54 +1,49 @@
-import { useState } from 'react'
-import { Link, Routes, Route } from 'react-router-dom'
-import Home from './Home'
-import EditRecipe from './EditRecipe'
-import Favorites from './Favorites'
-import Form from './Form'
-import Login from './Login'
-import MyRecipes from './MyRecipes'
-import Search from './Search'
-import Profile from './Profile'
-import RecipeList from './RecipeList'
-import SignUp from './SignUp'
+import { useState } from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import EditRecipe from './EditRecipe';
+import Favorites from './Favorites';
+import Form from './Form';
+import Login from './Login';
+import MyRecipes from './MyRecipes';
+import Search from './Search';
+import Profile from './Profile';
+import RecipeList from './RecipeList';
+import SignUp from './SignUp';
 
 function App() {
-
   return (
     <div>
-     <nav>
-      <Link to={"./"}>Home</Link>
-      <Link to={"./EditRecipe"}>EditRecipe</Link>
-      <Link to={"./Favorites"}>Favorites</Link>
-      <Link to={"./Form"}>Create New Recipe</Link>
-      <Link to={"./Login"}>Login</Link>
-      <Link to={"./MyRecipes"}>My Recipes</Link>
-      <Link to={"./Search"}>Search</Link>
-      <Link to={"./Profile"}>My Profile</Link>
-      <Link to={"./RecipeList"}>View Recipes</Link>
-      <Link to={"./SignUp"}>Sign Up</Link>
-      <Link to="./add-recipe">Add Recipe</Link>
-      <Link to="./my-recipes">My Recipes</Link>
-
-     </nav>
-     <main>
-      <Routes>
-        <Route path='./' element={<Home /> }/>
-        <Route path='./EditRecipe' element={<EditRecipe /> }/>
-        <Route path='./Favorites' element={<Favorites /> }/>
-        <Route path='./Form' element={<Form /> }/>
-        <Route path='./Login' element={<Login /> }/>
-        <Route path='./MyRecipes' element={<MyRecipes /> }/>
-        <Route path='./Search' element={<Search /> }/>
-        <Route path='./Profile' element={<Profile /> }/>
-        <Route path='./RecipeList' element={<RecipeList /> }/>
-        <Route path='./SignUp' element={<SignUp /> }/>
-        <Route path='./add-recipe' element={<Form />} />
-        <Route path='./my-recipes' element={<MyRecipes />} />
-      </Routes>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/add-recipe">Add Recipe</Link>
+        <Link to="/my-recipes">My Recipes</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/search">Search</Link>
+        <Link to="/profile">My Profile</Link>
+        <Link to="/recipe-list">View Recipes</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-recipe" element={<Form />} />
+          <Route path="/edit-recipe/:id" element={<EditRecipe />} />
+          <Route path="/my-recipes" element={<MyRecipes />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/recipe-list" element={<RecipeList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
       </main>
-
+      <footer>
+        <p>&copy; 2023 Recipe App</p>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
