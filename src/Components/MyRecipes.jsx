effc5a30cf3c6c0dc1964b23643ff95f57b0759c
mathiasmunene <mathiasmunene@gmail.com>
2025-06-02 12:39:05 +0300

    const Recipe

--- src/Components/MyRecipes.jsx
+++ src/Components/MyRecipes.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const MyRecipes = () => {
-  return <h1>MyRecipes Page</h1>;
-};
-
-export default MyRecipes;
+const MyRecipes = ({ customRecipes = [] }) => {
+  if (customRecipes.length === 0) {
+    return (
+      <div>
+        <h1>My Recipes</h1>
+        <p>You haven't created any recipes yet. <a href="/Form">Create your first recipe!</a></p>
+      </div>
+    );
+  }
