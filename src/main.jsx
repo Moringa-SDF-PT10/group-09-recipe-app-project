e16e067db9d23099d835fdf06ea9f582b2a7e176
mathiasmunene <mathiasmunene@gmail.com>
2025-06-02 14:40:03 +0300

    Alot of changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './Components/App';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './Components/App'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './Components/AuthContext'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <AuthProvider>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <AuthProvider>
       <App />
-      </AuthProvider>
-  </React.StrictMode>
-);
+    </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
