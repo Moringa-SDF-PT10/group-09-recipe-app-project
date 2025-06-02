deea914177e495d5df7b0bcbcc93139fa19ab288
Crispus Nganga <githogoringash91@gmail.com>
2025-06-02 07:49:19 +0300

    created RecipeItem

--- src/Components/RecipeList.jsx
+++ src/Components/RecipeList.jsx
@@ -1,56 +1,22 @@
-import {useState, useEffect} from "react";
+import React from "react";
+import RecipeItem from "./RecipeItem";
 
-const RecipeList = () => {
-  const[recipes, setRecipes] = useState([])
-  const [error, setError] = useState(null)
-  useEffect (() => {
-    fetch("https://dummyjson.com/recipes")
-    .then ((response) => response.json())
-    .then ((data) =>{
-       if (data.recipes && data.recipes.length > 0) {
-          setRecipes(data.recipes);
-        }
-    } )
-     .catch((err) => setError(err.message))
-
-  },[])
-  if (error) return <div>Error: {error}</div>
+const RecipeList = ({ recipes, onToggleFavorites, onEditRecipe, onDeleteRecipe }) => {
+    const recipeItems = recipes.map((recipe) =>
+    <RecipeItem
+       key={`custom-${recipe.id}`}
+       recipe={recipe}
+       onToggleFavorite={onToggleFavorites}
+       onEditRecipe={onEditRecipe}
+       onDeleteRecipe={onDeleteRecipe}
+    />)
   return (
-    <div>
-      <h2>Recipes</h2>
-      <ul>
-        {recipes.map((recipe, index) => (
-          <li key={recipe.id || index}>
-            <h3>{recipe.name}</h3>
-            <img
-              src={recipe.image}
-              alt={recipe.name}
-          
-            />
-            
-            <p>Ingredients: </p>
-            <ul>
-              {recipe.ingredients.map((item, i) => (
-                <li key={i}>{item}</li>
-              )
-              
-              )}
-            </ul>
-
-            
-            <p>Instructions: </p>
-            <ol>
-              {Array.isArray(recipe.instructions)
-              ? recipe.instructions.map((step, index) => (
-              <li key={index}>{step}</li>
-              )) : <li>No instructions available.</li>}
-            </ol>
-          </li>
-        ))}
-      </ul>
-    </div>
-
-
+    <section>
+        <h1>Recipes</h1>
+        <ul>{recipeItems}</ul>
+    </section>
   )
-}
+
+};
+
 export default RecipeList;
